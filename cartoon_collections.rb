0a7c3f017c5743f124747b46774cfa2cc6856c39
Devone Austin <devone.austin@gmail.com>
2020-04-18 20:18:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ def long_planeteer_calls(array)
   greater_than_four = []
   array.collect{|e| greater_than_four << e.split(" ").collect{|x| x.length}.join(" ")}
 
-  greater_than_four.all?{|e| e > 4}
+  greater_than_four.all?{|e| e.to_i > 4}
     binding.pry
     if greater_than_four.include? true
       true
