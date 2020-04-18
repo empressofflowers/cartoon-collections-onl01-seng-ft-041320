97cabdb5ebdedf9865015b47809ec2d495a7a9b1
Devone Austin <devone.austin@gmail.com>
2020-04-18 17:23:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.each.capitalize {|i| puts "#{i}!"}
+  array.collect.capitalize {|i| puts "#{i}!"}
 end
 
 def long_planeteer_calls# code an argument here
