50f42d245ad7d481270f15bee99faba23af1e688
Devone Austin <devone.austin@gmail.com>
2020-04-18 17:29:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,8 @@ end
 
 def summon_captain_planet(array)
   #binding.pry
-  array.collect {|i| i.capitalize}
-  array.each {|i| puts i + "!"}
+  array.collect {|i| put i.capitalize + "!"}
+  #array.each {|i| puts i + "!"}
 end
 
 def long_planeteer_calls# code an argument here
