f0c268a6c954760f152ba1bfb4f142a3fd2b41ca
Devone Austin <devone.austin@gmail.com>
2020-04-18 17:21:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,8 @@ def roll_call_dwarves(array)
   array.each_with_index { |i,index| puts "#{index + 1} #{i}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.each.upcase {|i| puts "#{i}!"}
 end
 
 def long_planeteer_calls# code an argument here
