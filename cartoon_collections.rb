806da9e807d420f08bf67ab09e1a7cee44b3f38f
Devone Austin <devone.austin@gmail.com>
2020-04-18 18:33:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 def summon_captain_planet(array)
   new_array = []
   array.collect.each {|i| new_array <<  i.capitalize }
-  new_array.collect {|i| another_array << puts i + "!"}
+  new_array.collect {|i| another_array << puts "|i| + "!"}
   another_array
 end
 
